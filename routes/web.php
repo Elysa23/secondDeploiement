ec2d8248ebd9f875dd052a978541ac45d25f72a1
Elysa23 <121961510+Elysa23@users.noreply.github.com>
2025-05-23 13:43:29 +0200

    fin test ajout route test pour API Key

--- routes/web.php
+++ routes/web.php
@@ -163,7 +163,4 @@ Route::get('/student/dashboard', [StudentDashboardController::class, 'dashboard'
     ->middleware(['auth', 'role:apprenant'])
     ->name('student.dashboard');
 
-// Route pour tester l'API
-Route::get('/test-cle', function () {
-    dd(env('MISTRAL_API_KEY'));
-});
+
